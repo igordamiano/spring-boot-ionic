30d5c85c81d9ae3239742871bfe71035744fa70f
Igor Damiano <igordamiano@gmail.com>
2021-01-26 10:40:57 -0300

    Projeto criado

---
+++ src/main/java/br/com/irdbr/springbootionic/SpringBootIonicApplication.java
@@ -0,0 +1,13 @@
+package br.com.irdbr.springbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/irdbr/springbootionic/SpringBootIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.irdbr.springbootionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
