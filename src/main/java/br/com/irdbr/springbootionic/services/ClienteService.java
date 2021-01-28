e2a0c8772008135ed755dad1e2f3322ac984797f
Igor Damiano <igordamiano@gmail.com>
2021-01-28 17:46:04 -0300

    endpoint /clientes/{id}

---
+++ src/main/java/br/com/irdbr/springbootionic/services/ClienteService.java
@@ -0,0 +1,26 @@
+package br.com.irdbr.springbootionic.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.irdbr.springbootionic.domain.Cliente;
+import br.com.irdbr.springbootionic.repositories.ClienteRepository;
+import br.com.irdbr.springbootionic.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Long id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+}
