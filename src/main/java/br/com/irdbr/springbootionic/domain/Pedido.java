39cae17dfbbde53127c938ed14879f2e7ce42f0c
Igor Damiano <igordamiano@gmail.com>
2021-01-29 15:38:00 -0300

    Correção - usando @JsonIgnore

--- src/main/java/br/com/irdbr/springbootionic/domain/Pedido.java
+++ src/main/java/br/com/irdbr/springbootionic/domain/Pedido.java
@@ -30,7 +30,6 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
@@ -38,7 +37,6 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
