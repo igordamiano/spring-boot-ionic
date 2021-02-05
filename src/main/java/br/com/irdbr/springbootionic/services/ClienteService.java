eff6ac41d53058134d84a71fa2b92aba0c23ab9c
Igor Damiano <igordamiano@gmail.com>
2021-02-05 13:32:48 -0300

    inserindo novo cliente com POST

--- src/main/java/br/com/irdbr/springbootionic/services/ClienteService.java
+++ src/main/java/br/com/irdbr/springbootionic/services/ClienteService.java
@@ -3,6 +3,8 @@ package br.com.irdbr.springbootionic.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.irdbr.springbootionic.domain.Cidade;
 import br.com.irdbr.springbootionic.domain.Cliente;
+import br.com.irdbr.springbootionic.domain.Endereco;
+import br.com.irdbr.springbootionic.domain.enums.TipoCliente;
 import br.com.irdbr.springbootionic.dto.ClienteDTO;
+import br.com.irdbr.springbootionic.dto.ClienteNewDTO;
+import br.com.irdbr.springbootionic.repositories.CidadeRepository;
 import br.com.irdbr.springbootionic.repositories.ClienteRepository;
+import br.com.irdbr.springbootionic.repositories.EnderecoRepository;
 import br.com.irdbr.springbootionic.services.exceptions.DataIntegrityException;
 import br.com.irdbr.springbootionic.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +29,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Long id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -62,6 +76,41 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli =  new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), 
+				TipoCliente.toEnum(objDto.getTipoCliente()));
+		
+		// Não usar cidadeRepository // Cidade cid = new Cidade(obj.getCidadeId(), null, null);
+		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), 
+				objDto.getBairro(), objDto.getCep(), cli, cid.orElse(null));
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
+	}
+
+	
+	@Transactional // Vai salvar os endereços na transação
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
 	}
 
 }
