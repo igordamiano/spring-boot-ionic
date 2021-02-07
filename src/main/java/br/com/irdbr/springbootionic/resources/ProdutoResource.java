ef8c620a2a23a347a5b1403441a7e45f71f8881f
Igor Damiano <igordamiano@gmail.com>
2021-02-07 12:26:20 -0300

    busca de produtos por nome e categoria

--- src/main/java/br/com/irdbr/springbootionic/resources/ProdutoResource.java
+++ src/main/java/br/com/irdbr/springbootionic/resources/ProdutoResource.java
@@ -1,13 +1,19 @@
 package br.com.irdbr.springbootionic.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.irdbr.springbootionic.domain.Produto;
+import br.com.irdbr.springbootionic.dto.ProdutoDTO;
+import br.com.irdbr.springbootionic.resources.utils.URL;
 import br.com.irdbr.springbootionic.services.ProdutoService;
 
 @RestController
@@ -24,5 +30,24 @@ public class ProdutoResource {
 		
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value = "nome", defaultValue = "") String nome,
+			@RequestParam(value = "categorias", defaultValue = "") String categorias, 
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction // ASC ou DESC
+			) {
+		String nomeDecoded = URL.decodeParam(nome);
+		List<Long> ids = URL.decodeLongList(categorias);
+		
+		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDTO = list.map(obj -> new ProdutoDTO(obj));
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 
 }
--- src/main/java/br/com/irdbr/springbootionic/services/ProdutoService.java
+++ src/main/java/br/com/irdbr/springbootionic/services/ProdutoService.java
@@ -1,12 +1,20 @@
 package br.com.irdbr.springbootionic.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.irdbr.springbootionic.domain.Categoria;
+import br.com.irdbr.springbootionic.domain.Pedido;
 import br.com.irdbr.springbootionic.domain.Produto;
+import br.com.irdbr.springbootionic.repositories.CategoriaRepository;
 import br.com.irdbr.springbootionic.repositories.ProdutoRepository;
+import br.com.irdbr.springbootionic.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ProdutoService {
@@ -14,10 +22,26 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repo;
 	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public Produto buscar(Long id) {
 		Optional<Produto> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
 	}
+	
+	public Page<Produto> search(String nome, List<Long> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		
+		//return repo.search(nome, categorias, pageRequest);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
+	
 	
 }
