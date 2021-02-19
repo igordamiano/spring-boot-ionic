1b270959b261c748acd14df46993aa09ffd05253
Igor Damiano <igordamiano@gmail.com>
2021-02-19 15:21:28 -0300

    usando padrao para imagens

--- src/main/java/br/com/irdbr/springbootionic/domain/Cliente.java
+++ src/main/java/br/com/irdbr/springbootionic/domain/Cliente.java
@@ -57,8 +57,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imgUrl;
-
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE); // por padrão, todos vão ter perfil de cliente
 	}
@@ -176,14 +174,6 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImgUrl() {
-		return imgUrl;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
 	}
 
 }
